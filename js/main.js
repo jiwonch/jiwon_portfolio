d30cb1715f21523bf8887f101920bfc8659e0e67
JIWON CHOI <rizjw26@gmail.com>
2022-08-18 16:18:45 +0900

    20220818_main_Bg

--- js/main.js
+++ js/main.js
@@ -69,4 +69,58 @@ $(".box").on("mousewheel DOMMouseScroll", function (e) {
     // $(".box").css({top:0},{position:fixed});
 });
 
+// typing animation
 
+const prH1d = document.querySelector('.pr_type');
+const prH1dText = 'FRONTEND DEVELOPER';
+
+const prH1n = document.querySelector('.pr_name');
+const prH1nText = "CHOI JI WON'S PORTFOLIO"
+
+let prd = 0;
+let prn = 0;
+
+function typing() {
+    if(prd < prH1dText.length)
+    {
+        let txt = prH1dText.charAt(prd);
+        prH1d.innerHTML += txt;
+        prd++;
+    }
+ 
+    if(prn < prH1nText.length)
+    {
+        let i = prH1nText.charAt(prn);
+        prH1n.innerHTML += i;
+        prn++;
+    }
+
+}
+
+// function typing_name(){
+//     if(prn < prH1nText.length)
+//     {
+//         let i = prH1nText.charAt(prn);
+//         prH1n.innerHTML += i == "\n" ? "<br/>" : i;
+//         prn++;
+//     }
+// }
+
+setInterval(typing, 200);
+// setInterval(typing_name, 300);
+
+
+// 첫번째 화면 배경
+var blendAmount = 70;
+var delay = -10;
+var windowWidth = window.innerWidth;
+var bg = document.getElementById('box_f1');
+
+document.onmousemove = function(e){
+    mouseX = Math.round(e.pageX / windowWidth * 100 - delay);
+
+    col1 = mouseX - blendAmount;
+    col2 = mouseX + blendAmount;
+
+    bg.style.background = "linear-gradient(to right, #7BC6CC " +col1 + "%, #BE93C5 "+ col2 + "%)";
+}
